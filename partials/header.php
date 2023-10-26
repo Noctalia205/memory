042ac05440298df531e9d82c59c73cc5c50e335f
Noctalia205 <carofil205@gmail.com>
2023-10-26 16:00:30 +0200

    hello

--- partials/header.php
+++ partials/header.php
@@ -1,18 +1,44 @@
 <?php require_once SITE_ROOT . ('utils/userConnexion.php');
 require_once SITE_ROOT . ('utils/database.php');
-?>
+
+if (isset($_SESSION["userID"])) : ?>
+    <?php $pdo = connectToDbAndGetPdo();
+    $pdoStatement = $pdo->prepare('SELECT username FROM Users WHERE id = :id');
+    $pdoStatement->execute([":id" => $_SESSION["userID"]]);
+    $userSessionId = $pdoStatement->fetch();
+    echo "test";
+    echo $userSessionId->username;
+    ?>
 
 
-<header>        <nav class="main-nav">
+    <header>
+        <nav class="main-nav">
             <div class="logo"><a href="index.php">Beginner SLAY Memory</a></div>
             <ul>
-                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>index.php" class="<?= $page == 'Accueil' ? 'active':'' ?> ">Accueil</a></li>
-                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>games/page_jeu_dbz.php" class="<?= $page == 'Jeu' ? 'active':'' ?> ">Jeu</a></li>
-                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>games/page_de_score.php" class="<?= $page == 'Score' ? 'active':'' ?> ">Score</a></li>
-                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>page_contact.php" class="<?= $page == 'Nous Contacter' ? 'active':'' ?> ">Nous contacter</a></li>
-                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>page_login.php" class="<?= $page == 'Connexion' ? 'active':'' ?> ">Connexion</a></li>
-                <div class="profil"><a href="<?php echo PROJECT_FOLDER ?>page_MyAccount.php"><img src="<?php echo PROJECT_FOLDER ;?>assets/icon-connexion.png" class="<?= $page == 'Pseudo' ? 'active':'' ?>" ></a><div class="contentt"><div class="textee"><?php echo $userUsername?></div></div></div>
+                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>index.php" class="<?= $page == 'Accueil' ? 'active' : '' ?> ">Accueil</a></li>
+                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>games/page_jeu_dbz.php" class="<?= $page == 'Jeu' ? 'active' : '' ?> ">Jeu</a></li>
+                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>games/page_de_score.php" class="<?= $page == 'Score' ? 'active' : '' ?> ">Score</a></li>
+                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>page_contact.php" class="<?= $page == 'Nous Contacter' ? 'active' : '' ?> ">Nous contacter</a></li>
+                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>page_login.php" class="<?= $page == 'Connexion' ? 'active' : '' ?> ">Connexion</a></li>
+                <div class="profil"><a href="<?php echo PROJECT_FOLDER ?>page_MyAccount.php"><img src="<?php echo PROJECT_FOLDER; ?>assets/icon-connexion.png" class="<?= $page == 'Pseudo' ? 'active' : '' ?>"></a></div>
             </ul>
         </nav>
 
-</header>
+    </header>
+<?php else : 
+    echo "déconnecté";?>
+    <header>
+        <nav class="main-nav">
+            <div class="logo"><a href="index.php">Beginner SLAY Memory</a></div>
+            <ul>
+                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>index.php" class="<?= $page == 'Accueil' ? 'active' : '' ?> ">Accueil</a></li>
+                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>games/page_jeu_dbz.php" class="<?= $page == 'Jeu' ? 'active' : '' ?> ">Jeu</a></li>
+                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>games/page_de_score.php" class="<?= $page == 'Score' ? 'active' : '' ?> ">Score</a></li>
+                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>page_contact.php" class="<?= $page == 'Nous Contacter' ? 'active' : '' ?> ">Nous contacter</a></li>
+                <li class="navpoint"><a href="<?php echo PROJECT_FOLDER ?>page_login.php" class="<?= $page == 'Connexion' ? 'active' : '' ?> ">Connexion</a></li>
+                <div class="profil"><a href="<?php echo PROJECT_FOLDER ?>page_MyAccount.php"><img src="<?php echo PROJECT_FOLDER; ?>assets/icon-connexion.png" class="<?= $page == 'Pseudo' ? 'active' : '' ?>"></a>Pseudo</div>
+            </ul>
+        </nav>
+
+    </header>
+<?php endif; ?>
