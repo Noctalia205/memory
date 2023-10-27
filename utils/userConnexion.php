f8923a7dc3d3ec5b891211ec9a5c2791529bd8ef
Noctalia205 <carofil205@gmail.com>
2023-10-27 11:57:12 +0200

    add

--- utils/userConnexion.php
+++ utils/userConnexion.php
@@ -1,7 +1,8 @@
 <?php /* CONNEXION USER */
 require_once SITE_ROOT . ('utils/database.php');
 
-function connexionUsers($userMail, $userPass, $userId, $userUsername): string {
+function connexionUsers($userMail, $userPass, $userId, $userUsername): string
+{
     $userMail = $_POST['mail'];
     $userPass = $_POST['mdp'];
 
@@ -10,19 +11,18 @@ function connexionUsers($userMail, $userPass, $userId, $userUsername): string {
     $pdoStatement = $pdo->prepare("SELECT id, username FROM Users WHERE mail='$userMail' AND pass='$userPass';");
     $pdoStatement->execute();
     $loginInfos = $pdoStatement->fetch();
-    if($pdoStatement->rowCount() < 1) return "utilisateur introuvable";
+    if ($pdoStatement->rowCount() < 1) return "utilisateur introuvable";
     $userId = $loginInfos->id;
-    
+
     if ($userId == null) {
         return 'Mail ou mot de passe incorrect ou inexistant';
-    }
-    else {
+    } else {
         $_SESSION["userID"] = $userId;
-        
+
         $pdoGetFromSession = $pdo->prepare("SELECT username FROM Users WHERE id = :id");
         $pdoGetFromSession->execute([":id" => $_SESSION["userID"]]);
         $pseudo = $pdoGetFromSession->fetch();
-        
+
         return "Bienvenu $pseudo->username !";
     }
 }
