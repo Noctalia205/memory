07d8668e6617c9321e01b7c301d1b62d404bc913
Vanessa <vanessa.boissierpro@gmail.com>
2023-10-27 09:05:54 +0200

    story 5-6

--- games/page_de_score.php
+++ games/page_de_score.php
@@ -27,45 +27,43 @@ $page = 'Score'; ?>
             </div>
         </div>
         <div class="barre_de_recherche_container">
-            <form method="$_GET">
+            <form class="search_form" method="$_GET">
                 <input name="BarreDeRecherche" placeholder="Tape ma bestie !">
+                <input type="submit" value="Rechercher" />
+            </form>
         </div>
-        <input type="submit" value="Rechercher" />
-        </form>
-        <div class="resultat_recherche">
+    </div>
+    <main class="table_score">
+        <div class="table_score_container">
+            <section class="table__header">
+                <h1>Tableau des scores</h1>
+            </section>
+            <section class="table__body">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th> Nom de l'utilisateur</th>
+                            <th>Nom du jeu</th>
+                            <th>Difficulté</th>
+                            <th>Score</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        if (isset($_GET['BarreDeRecherche']) && $_GET['BarreDeRecherche'] != "") {
 
+                            echo rechercheDeDonneesDansLaBarreDeRecherche();
+                        } else {
+                            echo recupereScorePageDeScore();
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </section>
         </div>
-        <main class="table_score">
-            <div class="table_score_container">
-                <section class="table__header">
-                    <h1>Tableau des scores</h1>
-                </section>
-                <section class="table__body">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>id</th>
-                                <th> Nom de l'utilisateur</th>
-                                <th>Nom du jeu</th>
-                                <th>Difficulté</th>
-                                <th>Score</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            if (isset($_GET['BarreDeRecherche']) && $_GET['BarreDeRecherche'] != "") {
-
-                                echo rechercheDeDonneesDansLaBarreDeRecherche();
-                            } else {
-                                echo recupereScorePageDeScore();
-                            }
-                            ?>
-                        </tbody>
-                    </table>
-                </section>
-            </div>
-        </main>
-        </body>
+    </main>
+    </body>
     </div>
 
     </div>
